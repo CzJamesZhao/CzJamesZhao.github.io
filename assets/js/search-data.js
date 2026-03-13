019e4450853794748d2ffcb9f192bf1e14678f28
CzJamesZhao <128059370+CzJamesZhao@users.noreply.github.com>
2026-03-13 06:42:49 +0000

    Deploying to gh-pages from @ CzJamesZhao/CzJamesZhao.github.io@af9a2edf978bc3ad74b67e71016680f61ca8bb74 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,16 +384,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-learning-to-leap-efficient-dense-point-tracking-by-focusing-where-it-matters-has-been-accepted-to-aaai-2026",
+          title: 'Our paper “Learning to LEAP: Efficient Dense Point Tracking by Focusing Where It...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
